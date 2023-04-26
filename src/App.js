05893e2f04288d6a76ae11ee4510cbf7aa442ed1
uday chauhan <chauhanuday842@gmail.com>
2023-04-26 16:53:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  
+  const [timer,settimer]=useState(0)
+  let timerid=useRef(0)
+
+
+  const startTimer=()=>{
+    timerid.current= setInterval(() => {
+        settimer((prev)=> prev+1)
+      }, 1000);
+  }
+
+  const stoptimer=()=>{
+    clearInterval(timerid.current)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+    <div className='counter'>
+      <h2>count value is {timer}</h2>
+
+      <div className='div'>
+      <button onClick={startTimer}>start</button>
+      <button onClick={stoptimer}>stop</button>
+      </div>
     </div>
+  
+    </>
   );
 }
 
